1e432f575845cfb43e97d73b39ed1dd7f1c7c170
luzo <luzoaki@gmail.com>
2023-02-01 21:57:34 -0300

    - Gerando Callbacks 1

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 import pandas as pd
 
 # import from folders/themes changer
-from dash_bootstrap_templates
+from dash_bootstrap_templates import ThemeSwitchAIO
 import dash
 
 FONT_AWESOME = ['https://use.fontawesome.com/releases/v5.10.2/css/all.css']
@@ -37,7 +37,7 @@ url_theme1 = dbc.themes.FLATLY
 url_theme2 = dbc.themes.DARKLY
 
 # ===== Reading n cleaning File ====== #
-df = pd.read_csv('dataset_asimov.csv')
+df = pd.read_csv('dataset_asimov.csv', sep=';')
 df_cru = df.copy()
 
 # Meses em numeros para poupar memória
@@ -56,6 +56,8 @@ df.loc[ df['Mês'] == 'Dez', 'Mês'] = 12
 
 # Algumas limpezas
 df['Valor Pago'] = df['Valor Pago'].str.lstrip('R$ ')
+df['Valor Pago'] = df['Valor Pago'].astype(str).str.replace('.','')
+df['Valor Pago'] = df['Valor Pago'].astype(int)
 df.loc[df['Status de Pagamento'] == 'Pago', 'Status de Pagamento'] = 1
 df.loc[df['Status de Pagamento'] == 'Não pago', 'Status de Pagamento'] = 0
 
@@ -66,14 +68,224 @@ df['Mês'] = df['Mês'].astype(int)
 df['Valor Pago'] = df['Valor Pago'].astype(int)
 df['Status de Pagamento'] = df['Status de Pagamento'].astype(int)
 
+# Criando opções pros filtros
+options_month = [{'label': 'Anoto todo', 'value':0}]
+for i, j in zip(df_cru['Mês'].unique(), df['Mês'].unique()):
+    options_month.append({'label': i, 'value': j})
+options_month = sorted(options_month, key=lambda x: x['value'])
+
+options_team = [{'label': 'Todas Equipes', 'values': 0}]
+for i in df['Equipe'].unique():
+    options_team.append(({'label': i, 'value':i}))
+
+# Função dos filtro
+def month_filter(month):
+    if month == 0:
+        mask = df['Mês'].isin(df['Mês'].unique())
+    else:
+        mask = df['Mês'].isin([month])
+    return mask
+
+def convert_to_text(month):
+    lista1 = ['Ano Todo','Janeiro','Fevereiro', 'Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
+    return lista1[month]
+
 #========= Layout =========#
 app.layout = dbc.Container(children=[
+    # Row 1
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dbc.Row([
+                        dbc.Col([
+                            html.Legend("Sales Analytics")
+                        ], sm=8),
+                        dbc.Col([
+                            html.I(className='fa fa-balance-scale', style={'font-size': '300%'})
+                        ], sm=4, align="center")
+                    ]),
+                    dbc.Row([
+                        dbc.Col([
+                            ThemeSwitchAIO(aio_id="theme", themes=[url_theme1, url_theme2]),
+                            html.Legend("Asimov Academy")
+                        ])
+                    ], style={'margin-top': '10px'}),
+                    dbc.Row([
+                        dbc.Button("Visite o Site", href="https://asimov.academy/", target="_blank")
+                    ], style={'margin-top': '10px'})
+                ])
+            ], style=tab_card)
+        ], sm=4, lg=2),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dbc.Row(
+                        dbc.Col(
+                            html.Legend('Top Consultores por Equipe')
+                        )
+                    ),
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Graph(id='graph1', className='dbc', config=config_graph)
+                        ], sm=12, md=7),
+                        dbc.Col([
+                            dcc.Graph(id='graph2', className='dbc', config=config_graph)
+                        ], sm=12, lg=5)
+                    ])
+                ])
+            ], style=tab_card)
+        ], sm=12, lg=7),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dbc.Row(
+                        dbc.Col([
+                            html.H5('Escolha o Mês'),
+                            dbc.RadioItems(
+                                id="radio-month",
+                                options=options_month,
+                                value=0,
+                                inline=True,
+                                labelCheckedClassName="text-success",
+                                inputCheckedClassName="border border-success bg-success",
+                            ),
+                            html.Div(id='month-select', style={'text-align': 'center', 'margin-top': '30px'},
+                                     className='dbc')
+                        ])
+                    )
+                ])
+            ], style=tab_card)
+        ], sm=12, lg=3)
+    ], className='g-2 my-auto', style={'margin-top': '7px'}),
+
+    # Row 2
+    dbc.Row([
+        dbc.Col([
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id='graph3', className='dbc', config=config_graph)
+                        ])
+                    ], style=tab_card)
+                ])
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id='graph4', className='dbc', config=config_graph)
+                        ])
+                    ],style=tab_card)
+                ])
+            ],className='g-2 my-auto', style={'margin-top': '7px'})
+        ], sm=12, lg=5),
+        dbc.Col([
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id='graph5', className='dbc', config=config_graph)
+                        ])
+                    ],style=tab_card)
+                ],sm=6),
+                dbc.Col([
+                    dbc.Card([
+                        dbc.CardBody([
+                            dcc.Graph(id='graph6', className='dbc', config=config_graph)
+                        ])
+                    ],style=tab_card)
+                ], sm=6)
+            ],className='g-2'),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Card([
+                        dcc.Graph(id='graph7', className='dbc', config=config_graph)
+                    ],style=tab_card)
+                ])
+            ], className='g-2 my-auto', style={'margin-top': '7px'})
+        ],sm=12, lg=4),
+        dbc.Col([
+            dbc.Card([
+                dcc.Graph(id='graph8', className='dbc', config=config_graph)
+            ],style=tab_card)
+        ],sm=12,lg=3)
+    ],className='g-2 my-auto', style={'margin-top': '7px'}),
+
+    # Row 3
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    html.H4("Distribuição de Propaganda"),
+                    dcc.Graph(id='graph9', className='dbc', config=config_graph)
+                ])
+            ], style=tab_card)
+        ],sm=12, lg=2),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    html.H4('valores de propagandas convertido por mês'),
+                    dcc.Graph(id='graph10', className='dbc', config=config_graph)
+                ])
+            ], style=tab_card)
+        ],sm=12, lg=5),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id='graph11', className='dbc', config=config_graph)
+                ])
+            ],style=tab_card)
+        ], sm=12, lg=3),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    html.H5('Escolha a equipe'),
+                    dbc.RadioItems(
+                        id='radio-team',
+                        options=options_team,
+                        value=0,
+                        inline=True,
+                        labelCheckedClassName='text-warning',
+                        inputCheckedClassName='border border-warning bg-warning',
+                    ),
+                    html.Div(id='team-select', style={'text-align': 'center', 'margin-top': '30px'},)
+                ])
+            ],style=tab_card)
+        ],sm=12, lg=2),
+    ], className='g-2 my-auto', style={'margin-top': '7px'})
 
 ],fluid=True, style={'height':'100vh'})
 
 #========= Callbacks =========#
+# Graficos 1 e 2
+@app.callback(
+    Output('graph1', 'figure'),
+    Output('graph2', 'figure'),
+    Output('month-select', 'children'),
+    Input('radio-month','value'),
+    Input(ThemeSwitchAIO.ids.switch('theme'),'value')
+)
+def graph1(month, toggle):
+    template = template_theme1 if toggle else template_theme2
 
+    mask = month_filter(month)
+    df_1 = df.loc[mask]
+
+    df_1 = df_1.groupby(['Equipe', 'Consultor'])['Valor Pago'].sum()
+    df_1 = df_1.sort_values(ascending=False)
+    df_1 = df_1.groupby('Equipe').head(1).reset_index()
+
+    fig2 = go.Figure(go.Pie(labels=df_1['Consultor'] + ' - ' + df_1['Equipe'], values=df_1['Valor Pago'], hole=0.6))
+    fig1 = go.Figure(go.Bar(x=df_1['Consultor'], y=df_1['Valor Pago'], textposition='auto', text=df_1['Valor Pago']))
+    fig1.update_layout(main_config, height=200, template=template)
+    fig2.update_layout(main_config, height=200, template=template, showlegend=False)
+
+    select = html.H1(convert_to_text(month))
+
+    return fig1, fig2, select
 
 # Run server
-if __name__ = '__main__' :
+if __name__ == '__main__':
     app.run_server(debug=True, port=8051)
